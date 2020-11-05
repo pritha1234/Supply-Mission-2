387105285beb0e07d6bf9bdb4825e8949420e942
pritha1234 <72921628+pritha1234@users.noreply.github.com>
2020-11-05 15:54:20 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,58 +1,68 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var engine, world;
-var box1,box2,box3,box4,box5;
-var log1,log2,log3,log4;
-var pig1,pig2;
-var ground;
-var bird;
-
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
-
-    ground = new Ground(600,height,1200,20);
-
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810,350);
-    log1 = new Log(810,260,300,PI / 2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig2 = new Pig(810,220);
-    log2 = new Log(810,180,300,PI / 2);
-
-    box5 = new Box(810,160,70,70);
-    log3 = new Log(760,120,150,PI / 7);
-    log4 = new Log(870,120,150, -PI / 7);
-
-    bird = new Bird(100,100);
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
 }
 
-function draw(){
-    background(0);
-    Engine.update(engine);
-    
-    ground.display();
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
 
-    box1.display();
-    box2.display();
-    pig1.display();
-    log1.display();
 
-    box3.display();
-    box4.display();
-    pig2.display();
-    log2.display();
+	packageSprite=createSprite(width/2, 200, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
 
-    box5.display();
-    log3.display();
-    log4.display();
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
 
-    bird.display();
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	box1 = createSprite(width/2, height-40, 160, 10);
+	box1.shapeColor = ("red");
+	box2 = createSprite(width/2+80, height-65, 10, 60);
+	box2.shapeColor = ("red");
+	box3 = createSprite(width/2-80, height-65, 10, 60);
+	box3.shapeColor = ("red");
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	World.add(world, packageBody);
+
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+
 }
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+	Matter.Body.setStatic(packageBody,false);
+	packageBody.restitution = 0.5
+ }
+
+	}
